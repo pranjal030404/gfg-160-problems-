ed1cf8fd0fe5edceffb1ea35cb0919a0292aa1f5
pranjal030404 <pranjalp160@gmail.com>
2025-03-28 22:42:01 +0530

    next permutation

---
+++ nextPermutation.java
@@ -0,0 +1,41 @@
+// Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
+
+// Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+
+
+class Solution {
+    void nextPermutation(int[] arr) {
+        // code here
+         int n = arr.length;
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the right
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If such an element is found, swap it with the next larger element
+        if (i >= 0) {
+            int j = n - 1;
+            while (arr[j] <= arr[i]) {
+                j--;
+            }
+            swap(arr, i, j);
+        }
+
+        // Step 3: Reverse the right portion to get the next permutation
+        reverse(arr, i + 1, n - 1);
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            swap(arr, left++, right--);
+        }
+    }
+}
